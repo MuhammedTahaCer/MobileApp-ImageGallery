da3d486a42e49235ab31946e784bfe93b761fc93
muhammed cer <cermuhammed720@gmail.com>
2023-04-23 12:28:49 +0300

    Project was Created

---
+++ app/src/main/java/com/muhammed/mobileapp_imagegallery/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.muhammed.mobileapp_imagegallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
