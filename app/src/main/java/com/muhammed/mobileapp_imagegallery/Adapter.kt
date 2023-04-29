fb5104ff363a0bb528cd17899aedd5db0d76057b
MuhammedTahaCer <cermuhammed720@gmail.com>
2023-04-29 15:25:40 +0300

    List the Categories

--- app/src/main/java/com/muhammed/mobileapp_imagegallery/Adapter.kt
+++ app/src/main/java/com/muhammed/mobileapp_imagegallery/Adapter.kt
@@ -11,9 +11,9 @@ import com.bumptech.glide.Glide
 
 class Adapter:BaseAdapter {
 
-    private  var layoutInflater:LayoutInflater?=null
-    private  var context:Context?=null
-    private  var categories:ArrayList<Category>?=null
+    private var layoutInflater:LayoutInflater?=null
+    private var context:Context?=null
+    private var categories:ArrayList<Category>?=null
 
     constructor(){
 
@@ -37,14 +37,14 @@ class Adapter:BaseAdapter {
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view= layoutInflater!!.inflate(R.layout.categorylineview, null)
-        val twHead= view.findViewById<TextView>(R.id.twCategory)
+        val v= layoutInflater!!.inflate(R.layout.categorylineview, null)
+        val twHead= v.findViewById<TextView>(R.id.twCategory)
         //Glide ekledikten sonra
-            val ivImages=view.findViewById<ImageView>(R.id.imgCategory)
+            val ivImages=v.findViewById<ImageView>(R.id.imgCategory)
             //glide usages exm : Glide.with(this).load("https://goo.gl/gEgYUd").into(imageView);
-            Glide.with(view.context).load(categories!!.get(position).imgUrl).into(ivImages);//Activity sınıfı içinde olmadığımız için this ifadesini kabul etmiyor. O halde konumunu tvermeli
+            Glide.with(v.context).load(categories!![position].imgUrl).into(ivImages);//Activity sınıfı içinde olmadığımız için this ifadesini kabul etmiyor. O halde konumunu tvermeli
         //Glide ekledikten sonra
         twHead!!.text=categories!![position].head
-        return view
+        return v
     }
 }
--- app/src/main/java/com/muhammed/mobileapp_imagegallery/MainActivity.kt
+++ app/src/main/java/com/muhammed/mobileapp_imagegallery/MainActivity.kt
@@ -2,10 +2,31 @@ package com.muhammed.mobileapp_imagegallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity() {
+    var adapterCtg: Adapter?=null
+    var list= ArrayList<Category>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val lvCategory=findViewById<ListView>(R.id.lvCategory)
+
+        //listeye eleman:
+        list.add(Category(1,"Flower","https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_960_720.jpg"))
+        list.add(Category(2,"Nature","https://cdn.pixabay.com/photo/2023/04/25/15/08/church-7950418_960_720.jpg"))
+        //list.add(Category(3,"Tech",""))
+        //list.add(Category(4,"Healt",""))
+        //list.add(Category(5,"Sport",""))
+        list.add(Category(6,"Landscape","https://cdn.pixabay.com/photo/2023/04/25/18/14/mountain-7950729_960_720.jpg"))
+        adapterCtg=Adapter(applicationContext,list)
+
+        lvCategory.adapter = adapterCtg // //Adapter içine göndereceğimiz dosyaları MainActivity'e tanımlıyacağız
     }
+
+    /*https://github.com/bumptech/glide
+    * Glide kütüphanesi:
+    * **Settings.gradle 'a mavenCentral() kodu eklenip sync edilir
+    * **Build.gradle(Module) 'a implement kodu eklenir ve sync edilir. Artık Glide ile internetten veya cihaz üzerindeki herhangi contenti açmaya yarıyacak önbellek işlemleridir.*/
+
 }
